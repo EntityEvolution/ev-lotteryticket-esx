b6a65a37176427ca2f9625c50191bf27fd566b9b
Monte <76192660+xMontesito@users.noreply.github.com>
2021-07-21 23:34:00 -0500

    feat(core): initial push

---
+++ lotteryticket/config.lua
@@ -0,0 +1,5 @@
+Config = {}
+Config.Locale  = 'en' -- Language
+Config.jackpot = '1000000'
+Config.jackpot2 = '1000000'
+Config.jackpot3 = '1000000'
---
+++ lotteryticket/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Monte' 
+description 'Lottery ticket'
+version '0.1.0'
+
+server_script 'server/server.lua'
